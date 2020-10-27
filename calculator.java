dab9cc9ac83782763f67e0834342b8dd51a03fd4
Prakhar Dubey <prakhar.dub@gmail.com>
2020-10-27 11:08:40 +0530

    Update calculator.java
    indented the do while loop
    indented all the cases
    added a case for division "/"

--- calculator.java
+++ calculator.java
@@ -7,32 +7,36 @@ class calculator
 		String c,s;
 		Scanner z=new Scanner(System.in);
 		do{
-		System.out.print("enter first number:");
-		a=z.nextInt();
-		System.out.print("enter second number:");
-		b=z.nextInt();
-		System.out.print("enter symbol(+,-,*):");
-		c=z.next();
-		switch(c)
-		{
-			case "+":
-			d=a+b;
-			System.out.print(d);
-			break;
-			case "-":
-			d=a-b;
-			System.out.print(d);
-			break;
-			case "*":
-			d=a*b;
-			System.out.print(d);
-			break;
-			default:
-			System.out.print("wrong choice!");
-			break;
-		}
-		System.out.print("\ndo u want to continue?press y to continue!!");
-		s=z.next();
+			System.out.print("enter first number:");
+			a=z.nextInt();
+			System.out.print("enter second number:");
+			b=z.nextInt();
+			System.out.print("enter symbol(+,-,*,/):");
+			c=z.next();
+			switch(c)
+			{
+				case "+":
+					d=a+b;
+					System.out.print(d);
+					break;
+				case "-":
+					d=a-b;
+					System.out.print(d);
+					break;
+				case "*":
+					d=a*b;
+					System.out.print(d);
+					break;
+				case "/":
+					d=a/b;
+					System.out.print(d);
+					break;
+				default:
+					System.out.print("wrong choice!");
+					break;
+			}
+			System.out.print("\ndo u want to continue?press y to continue!!");
+			s=z.next();
 		}while(s.equals("y"));
 	}
 }
